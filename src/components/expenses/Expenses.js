22fa3d3a97c43271f15482f252348214f10eba2d
Rui En <neoruien123@gmail.com>
2021-06-17 14:05:54 +0800

    First commit

---
+++ src/components/expenses/Expenses.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../ui/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+
+function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState("2019");
+  const selectedYearChangeHandler = (newlySelectedYear) => {
+    setSelectedYear(newlySelectedYear);
+  };
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={selectedYear}
+          onChangeSelectedYear={selectedYearChangeHandler}
+        />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
---
+++ src/components/newExpense/ExpenseForm.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+function ExpenseForm(props) {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
+  const titleChangedHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // use this if you want to update state that depends on the previous state
+    // wrong example:
+    // const [counter, setCounter] = useState(1);
+    // setCounter(counter + 1);
+    // correction:
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangedHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangedHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault(); // prevent page refresh
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangedHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangedHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangedHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
